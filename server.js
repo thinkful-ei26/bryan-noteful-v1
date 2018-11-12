36501f2cfcd08d82a715d1b431dc575bb82fbc21
Fantosism <businessspamfolder@gmail.com>
2018-11-12 13:48:08 -0700

    added get request for named route parameter

--- server.js
+++ server.js
@@ -11,8 +11,22 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public'));
+
+app.get('/api/notes', (request, response) => {
+  response.json(data);
+});
+
+app.get('/api/notes/:id', (request, response) => {
+// console.log('request params iceCream =', request.params.iceCream);
+  let noteObject = data.find(item => item.id === Number(request.params.id));
+  response.json(noteObject);
+});
+
 app.listen(8000, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
 });
+
+
