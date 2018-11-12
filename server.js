cdcaf61400d645537f5974959418039304324975
Fantosism <businessspamfolder@gmail.com>
2018-11-12 12:21:16 -0700

    added start script, created express app with basic listening logs

--- server.js
+++ server.js
@@ -6,3 +6,13 @@ const data = require('./db/notes');
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+
+const express = require('express');
+
+const app = express();
+
+app.listen(8000, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
