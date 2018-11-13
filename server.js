f99eda83e17677f5ffc9b7f3df5d52c1de7ced09
Fantosism <businessspamfolder@gmail.com>
2018-11-12 18:14:36 -0700

    fixed search logic for title and content

--- server.js
+++ server.js
@@ -14,11 +14,19 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/api/notes', (request, response) => {
-  response.json(data);
+  let request1 = request.query.searchTerm;
+
+  function titleContains(searchResults) {
+    return (searchResults.title.toLowerCase().includes(this) || searchResults.content.toLowerCase().includes(this));
+  }
+  // this in this context refers to "request 1" on line 24. .filter(function, thisValue). this refers to this value in context of .filter.
+
+  let results = data.filter(titleContains, request1); 
+  response.json(results);
 });
 
 app.get('/api/notes/:id', (request, response) => {
-// console.log('request params iceCream =', request.params.iceCream);
+  // console.log('request params2 is', request.params);
   let noteObject = data.find(item => item.id === Number(request.params.id));
   response.json(noteObject);
 });
